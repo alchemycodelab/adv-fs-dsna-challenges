491c7d4c4e43cacbf1eb95414cfedba3172cfb6c
Julie Nisbet <jmnisbet@gmail.com>
2022-09-18 16:36:41 -0700

    get examples ready

--- day-01/index.js
+++ day-01/index.js
@@ -1,2 +1,7 @@
-function reverseSentence() {}
+function reverseSentence(str) {
+  return str
+    .split(' ')
+    .map((word) => word.split('').reverse().join(''))
+    .join(' ');
+}
 module.exports = { reverseSentence };
